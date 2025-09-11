540b4103080d6c5f06fd0a5cac917861c30cdc1f
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-11 23:14:58 +0530

    .

---
+++ hashing/hashing_map.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <map>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a no of element to insert :";
+    cin>>n;
+    int arr[n];
+    cout << "Enter " << n << " numbers: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    int q;
+    cout<<"Enter a number of character for quries :"<<endl;
+    cin>>q;
+    cout<<"Enter "<<q<< "number "<<endl;
+    while(q--){
+        int x;
+        cin>>x;
+        cout << "Frequency of " << x << " = " << mpp[x] << endl;
+    }
+
+
+}
