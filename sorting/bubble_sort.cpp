46ea6f4d74d8bf360e2458ea67bce99c531ac2e4
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-13 18:36:00 +0530

    .

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void bubble_sort(int arr[],int n){
     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){       //in 1st itreation 
+        for(int j=0;j<n-1;j++){       
             if(arr[j]>arr[j+1]){
 
                 int temp = arr[j];
