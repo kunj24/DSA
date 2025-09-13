551c2b3d3ce9a22ee898f2857eb1fa2fcd7b7e64
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-13 18:45:09 +0530

    .

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 void bubble_sort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){       
-            if(arr[j]>arr[j+1]){
-
+    for(int i=0;i<n-1;i++){             //Largest element goes to the end at each -> 
+        for(int j=0;j<n-i-1;j++){      //-> itreationso ignore the last element so n-i-1
+            if(arr[j]>arr[j+1]){        
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
