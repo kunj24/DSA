cac9c585cbb5fda4b8c672b76bed26645b78e328
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-16 01:14:58 +0530

    .

---
+++ Problems on array/second_large_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>  // Needed for sort();
+using namespace std;
+int sortArr(vector<int> &arr){
+    sort(arr.begin(),arr.end());            
+    return arr[arr.size()-2];
+
+}
+int main(){
+    int n;
+    cout<<"Enter a number of element :";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter numbers :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"largest element is :"<<sortArr(arr);
+}    
