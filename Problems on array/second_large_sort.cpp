0be1e6940a238e74d3040ee1603cff2c319c8c1b
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-16 01:25:20 +0530

    .

--- Problems on array/second_large_sort.cpp
+++ Problems on array/second_large_sort.cpp
@@ -2,19 +2,20 @@
 #include<vector>
 #include<algorithm>  // Needed for sort();
 using namespace std;
-int sortArr(vector<int> &arr){
+void sortArr(vector<int> &arr){
     sort(arr.begin(),arr.end());  //this only work when array last 2 element is unique
-    return arr[arr.size()-2];
-
+    cout<<"largest elemnt is : "<<arr[arr.size()-1]<<endl;
+    cout<<"2nd largest elment is : "<<arr[arr.size()-2];
+    // 1,2,3,4,4 so largest=4 2nd largest=4 but we want 2nd largest is 3
 }
 int main(){
     int n;
     cout<<"Enter a number of element :";
     cin>>n;
     vector<int> arr(n);
-    cout<<"Enter numbers :"<<endl;
+    cout<<"Enter numbers unique number :"<<endl;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"largest element is :"<<sortArr(arr);
+    sortArr(arr);
 }    
