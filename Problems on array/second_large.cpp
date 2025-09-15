269fe217cb47113a4fbc29b331d09963ac09340f
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-16 01:40:20 +0530

    .

--- Problems on array/second_large.cpp
+++ Problems on array/second_large.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int secondlarge(int arr[],int n){
     int large=arr[0];
     int secondlarge=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>large){
-            secondlarge=large;
+    for(int i=0;i<n;i++){       //in that case if we 1,2,3,5,5
+        if(arr[i]>large){       //then it give 3 as 2nd largest 
+            secondlarge=large;  //in sorting it give 5 as 2nd largest
             large=arr[i];
         }
         else if(arr[i] < large && arr[i] > secondlarge) {
