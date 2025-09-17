a828fa05fe63a693faa54659a8cc15c04df0f227
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-17 20:47:29 +0530

    .

---
+++ Problems on array/arr_is_sorted.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+bool issorted(int arr[],int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int n;
+    cout<<"Enter a number of element :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter numbers :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    issorted(arr,n);
+
+}    
