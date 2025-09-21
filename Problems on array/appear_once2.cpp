e70e277fb5b0030f9f558f7fd4c3a8d70a9b0420
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-22 00:19:49 +0530

    .

--- Problems on array/appear_once2.cpp
+++ Problems on array/appear_once2.cpp
@@ -7,15 +7,15 @@ int appear(int arr[],int n){
     int max_val=0;
     for(int i=0;i<n;i++){       //1 1 2 3 3
         max_val=max(max_val,arr[i]);   // max_val =3
-    }
-    vector<int> hash(max_val + 1, 0);
+    }                                 //max is 3 so hash[0,1,2,3] so hash is 4   
+    vector<int> hash(max_val + 1, 0);   //hash of 4 all are 0
 
     for(int i=0;i<n;i++){
-        hash[arr[i]]++;
+        hash[arr[i]]++;     // hash[1]=2,[2]=1,[3]=2
     }
 
     for(int i=0;i<=max_val;i++){
-        if(hash[i]==1){
+        if(hash[i]==1){     
             return i;
         }
     }
