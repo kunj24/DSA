64fdf018767792cf14ab36a034d1a603855b0024
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-22 00:44:27 +0530

    .

--- Problems on array/appear_once3.cpp
+++ Problems on array/appear_once3.cpp
@@ -3,11 +3,11 @@
 #include<algorithm>
 using namespace std;
 int appear(int arr[],int n){
-    int xor1=0; 
+    int xor1=0; //1 1 2 3 3 
     for(int i=0;i<n;i++){
-        xor1=xor1^arr[i];
-    }
-    return xor1;
+        xor1=xor1^arr[i]; //0^1 then 1^1=0 then 0^2=2 then 2^3=1 then 1^3=2
+    }  // in sort 0 ^ 1 ^ 1 ^ 2 ^ 3 ^ 3 =(1 ^ 1) ^ (3 ^ 3) ^ 2 ^ 0 = 0 ^ 0 ^ 2 ^ 0 = 2                    
+    return xor1; // so result is 2 
 }
 int main(){
     int n;
