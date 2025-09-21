39072fe54783c98f6391476880018027a8131a96
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-22 00:17:29 +0530

    .

---
+++ Problems on array/appear_once2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int appear(int arr[],int n){
+    if(n == 0) return -1;
+    int max_val=0;
+    for(int i=0;i<n;i++){       //1 1 2 3 3
+        max_val=max(max_val,arr[i]);   // max_val =3
+    }
+    vector<int> hash(max_val + 1, 0);
+
+    for(int i=0;i<n;i++){
+        hash[arr[i]]++;
+    }
+
+    for(int i=0;i<=max_val;i++){
+        if(hash[i]==1){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cout<<"Enter a number of element :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter numbers in all number in twice execpt one :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int val=appear(arr,n);
+    cout<<"The signle element is :"<<val;
+
+}    
