01d8ba9eaaa56437765cd1c7c7dc543197448663
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-21 18:12:30 +0530

    .

---
+++ Problems on array/missing_number2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int missing_num(vector<int>& arr, int n){
+    int totalsum=0;
+    for(int i=0;i<n-1;i++){
+        totalsum+=arr[i];
+    }
+    int arrsum= ((n)*(n+1))/2;
+    return arrsum-totalsum;
+
+}
+
+int main(){
+    int n;
+    cout << "Enter the maximum number n: ";
+    cin >> n;
+    vector<int> arr(n - 1); // Array should have n-1 elements
+    
+    cout << "Enter " << n - 1 << " numbers from 1 to " << n << " in sequence :" << endl;
+    for(int i = 0; i < n - 1; i++){
+        cin >> arr[i];
+    }
+    
+    int val = missing_num(arr, n);
+    cout << "Missing number: " << val;
+    
+    return 0;
+}
