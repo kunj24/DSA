f568efecdc9852dc50816ff594e54f6e6ec05c20
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-21 21:33:10 +0530

    .

--- Problems on array/missing_nummber3.cpp
+++ Problems on array/missing_nummber3.cpp
@@ -2,14 +2,14 @@
 #include<vector>
 using namespace std;
 int missing_num(vector<int>& arr, int n){
-    int N=n-1;
-    int xor1,xor2=0;
-    for(int i=0;i<N;i++){
-        xor2=xor2^arr[i];
-        xor1=xor1^arr[i+1];
-    }
-    xor1=xor1^n;
-    return xor1^xor2;
+    int N=n-1;      //n = 5 and array [1, 2, 4, 5] (missing 3)
+    int xor1,xor2=0; //i=0: xor2 = 0^1 = 1, xor1 = 0^1 = 1 
+    for(int i=0;i<N;i++){ //i=1: xor2 = 1^2 = 3, xor1 = 1^2 = 3  
+        xor2=xor2^arr[i];  //i=2: xor2 = 3^4 = 7, xor1 = 3^3 = 0
+        xor1=xor1^(i+1);   //i=3: xor2 = 7^5 = 2, xor1 = 0^4 = 4
+    }                   
+    xor1=xor1^n;    //xor1 = 4 ^ 5 = 1  (XOR with n=5)
+    return xor1^xor2;       // 1^2 =3
 }
 
 int main(){
