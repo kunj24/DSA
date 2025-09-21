3f2108de42aff80e0be979f16248e5caa6fb9ff1
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-21 17:52:34 +0530

    .

--- Problems on array/missing_number1.cpp
+++ Problems on array/missing_number1.cpp
@@ -2,11 +2,11 @@
 #include<vector>
 using namespace std;
 int missing_num(vector<int>& arr, int n){
-    int hash[n+1]={0};
-    for(int i=0;i<n;i++){
-        hash[arr[i]]++;
-    }
-    for(int i=1;i<=n;i++){
+    int hash[n+1]={0};  //declare the n+1 size array
+    for(int i=0;i<n;i++){   // 1 2 4 5 
+        hash[arr[i]]++;     //hash[0,3]=0
+    }                       //hash[1,2,4,5]=1        
+    for(int i=1;i<=n;i++){  //starting from 1 so index 3 is 0 so missing is 3
         if(hash[i]==0){
             return i;
         }
@@ -21,7 +21,7 @@ int main(){
     cin >> n;
     vector<int> arr(n - 1); // Array should have n-1 elements
     
-    cout << "Enter " << n - 1 << " numbers from 1 to " << n << "in sequence :" << endl;
+    cout << "Enter " << n - 1 << " numbers from 1 to " << n << " in sequence :" << endl;
     for(int i = 0; i < n - 1; i++){
         cin >> arr[i];
     }
