8d8dbbece1a10ec5597f5b768423e3c4f20a58dc
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-09-22 22:34:27 +0530

    .

--- Problems on array/Esay/longest_subarr_sumk.cpp
+++ Problems on array/Esay/longest_subarr_sumk.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 int longest_subarr_sumofk(vector<int>& arr, int n,int k){
     int len=0;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int s=0;
+    for(int i=0;i<n;i++){   //2 3 5 1 9 sum=10 output is 3 2+3+5(maximum not 9+1 minimum)
+        for(int j=i;j<n;j++){   //start from i 
+            int s=0;    //every time sum=0
             for(int x = i; x <= j; x++){  
-                s += arr[x];
+                s += arr[x];    //first time s=2 then 5 then 10
             }
-            if(s == k) {
-                len = max(len, j - i + 1);
-            }
-        }
+            if(s == k) {        //if sum=k
+                len = max(len, j - i + 1);  //then len=max(length and j-i+1(3-1+1)) 
+            }       //suppose if we cover new len then compaer the len and j-i+1
+        }       //like we cover the k=10 at 9+1 then max(3,j-i+1,4-3+1=2)
     }
     return len;
 }
